7c9ae5987d59ec05ff171f5dc1bc426c1a1332a1
Daniyal Idrees <daniyal.idrees@sprind.org>
2025-03-07 22:53:54 +0100

    ui module setup

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.ui"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         minSdk = 24
@@ -33,6 +33,7 @@ android {
 }
 
 dependencies {
+    implementation(project(":domain"))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
