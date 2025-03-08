b51b21c322f2af684985167fc581386d885da61b
Daniyal Idrees <daniyal.idrees@sprind.org>
2025-03-08 21:14:42 +0100

    loading view added

---
+++ ui/src/main/java/com/example/ui/common/view/LoadingView.kt
@@ -0,0 +1,35 @@
+package com.example.ui.common.view
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+/**
+ * Internal use only.
+ *
+ * A composable that displays a loading indicator.
+ *
+ * @param modifier The modifier to be applied to the Box.
+ */
+@Composable
+internal fun LoadingView(modifier: Modifier) {
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center,
+    ) {
+        CircularProgressIndicator(
+            color = MaterialTheme.colorScheme.onSurface,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun LoadingViewPreview() {
+    LoadingView(Modifier.fillMaxSize())
+}
