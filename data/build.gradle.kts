2d0fe3573827c9fa6bae391a9ffc62c9f1ba72b6
Daniyal Idrees <daniyal.idrees@sprind.org>
2025-03-08 14:12:30 +0100

    data build gradle update with dependency of test feature module

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -20,9 +20,6 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
 
         buildConfigField("String", "API_KEY", apiKey)
@@ -50,17 +47,10 @@ android {
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-
     implementation(libs.retrofit.core)
     implementation(libs.retrofit.gson)
 
     api(libs.hilt.android)
 
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    testImplementation(project(":test-feature"))
 }
