96db098d81793c784b844a7b53c3ae0f571fd203
Daniyal Idrees <daniyal.idrees@sprind.org>
2025-03-08 06:42:55 +0100

    refactoring the repo photo search result

--- data/src/main/java/com/example/data/RepoPhotoSearchResult.kt
+++ data/src/main/java/com/example/data/RepoPhotoSearchResult.kt
@@ -6,6 +6,6 @@ import com.example.data.dto.PhotoItemDto
 sealed interface RepoPhotoSearchResult {
     data class Success(val photos: List<PhotoItemDto>) : RepoPhotoSearchResult
     data object Error : RepoPhotoSearchResult
-    data object NoInternetConnection : RepoPhotoSearchResult
+    data object NetworkUnavailable : RepoPhotoSearchResult
     data object InvalidStatus : RepoPhotoSearchResult
 }
