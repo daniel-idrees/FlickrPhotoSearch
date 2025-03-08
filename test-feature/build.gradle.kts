e47fb9959b752c810fa5775ed8e9ff609890559a
Daniyal Idrees <daniyal.idrees@sprind.org>
2025-03-08 06:42:31 +0100

    test feature module added with dependencies and test rule

---
+++ test-feature/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.example.domain"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+    api(libs.junit)
+    api (libs.mockito.kotlin)
+    api(libs.mockito.core)
+    api(libs.mockito.inline)
+    api( libs.kotlinx.coroutines.test)
+    api (libs.kotest.runner.junit5)
+    api (libs.kotest.assertions.core)
+    api (libs.kotest.property)
+    api(libs.turbine)
+
+    api(libs.androidx.core.ktx)
+
+
+    api(libs.androidx.junit)
+    api (libs.mockito.android)
+}
