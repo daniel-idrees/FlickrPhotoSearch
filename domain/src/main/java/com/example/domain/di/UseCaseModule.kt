1fbaf425b9dfd9c95442a0b777eb58c97be7b9ca
Daniyal Idrees <daniyal.idrees@sprind.org>
2025-03-10 00:54:02 +0100

    di fix for use case module

--- domain/src/main/java/com/example/domain/di/UseCaseModule.kt
+++ domain/src/main/java/com/example/domain/di/UseCaseModule.kt
@@ -1,20 +1,21 @@
 package com.example.domain.di
 
+import com.example.data.repository.PhotoRepository
 import com.example.domain.usecase.GetPhotoListUseCase
 import com.example.domain.usecase.GetPhotoListUseCaseImpl
-import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
-import javax.inject.Singleton
+import dagger.hilt.android.scopes.ViewModelScoped
 
 
 @Module
 @InstallIn(ViewModelComponent::class)
-internal interface UseCaseModule {
-    @Binds
-    @Singleton
-    fun providesUseCase(
-        impl: GetPhotoListUseCaseImpl,
-    ): GetPhotoListUseCase
+internal class UseCaseModule {
+    @Provides
+    @ViewModelScoped
+    fun providesGetPhotoListUseCase(
+        photoRepository: PhotoRepository
+    ): GetPhotoListUseCase = GetPhotoListUseCaseImpl(photoRepository)
 }
