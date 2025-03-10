e767cb688593a6b1322618062ae6917dc74ffe9b
Daniyal Idrees <daniyal.idrees@sprind.org>
2025-03-10 09:16:42 +0100

    old file removed

--- domain/src/main/java/com/example/domain/RepoPhotoSearchResult.kt
+++
@@ -1,9 +0,0 @@
-package com.example.domain
-
-import com.example.domain.model.PhotoItem
-
-sealed interface PhotoSearchResult {
-    data class Success(val photos: List<PhotoItem>) : PhotoSearchResult
-    data class Error(var errorMessage: String) : PhotoSearchResult
-    data object Empty : PhotoSearchResult
-}
