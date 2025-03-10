581a858717cc4c7a30fb6b3658c7a28aa2b24838
Daniyal Idrees <daniyal.idrees@sprind.org>
2025-03-10 09:17:48 +0100

    button text property removed from error config

--- ui/src/main/java/com/example/ui/common/content/ContentErrorConfig.kt
+++ ui/src/main/java/com/example/ui/common/content/ContentErrorConfig.kt
@@ -4,6 +4,5 @@ package com.example.ui.common.content
 data class ContentErrorConfig(
     val errorTitle: String,
     val errorSubTitle: String,
-    val retryButtonText: String? = null,
     val onRetry: (() -> Unit)? = null
 )
