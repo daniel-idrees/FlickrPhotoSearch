9b867388e5bad03d270c1b89e3c1c8fb030dfbc2
Daniyal Idrees <daniyal.idrees@sprind.org>
2025-03-10 21:08:25 +0100

    Top Left arrow icon rotation added in the view

--- ui/src/main/java/com/example/ui/main/bottomtabs/view/TopLeftArrowIcon.kt
+++ ui/src/main/java/com/example/ui/main/bottomtabs/view/TopLeftArrowIcon.kt
@@ -5,6 +5,7 @@ import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 
 
 @Composable
@@ -12,6 +13,8 @@ internal fun TopLeftArrowIcon(modifier: Modifier) {
     Icon(
         imageVector = Icons.AutoMirrored.Default.ArrowBack,
         contentDescription = "Top-left arrow icon",
-        modifier = modifier
+        modifier = modifier.graphicsLayer {
+            rotationZ = 45f
+        }
     )
 }
