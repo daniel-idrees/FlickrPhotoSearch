6544712d783f91c413c7838374a9147921d67f5c
Daniyal Idrees <daniyal.idrees@sprind.org>
2025-03-10 13:58:04 +0100

    photo list implementation improved with placeholder, spacing separate icon views

--- ui/src/main/java/com/example/ui/main/bottomtabs/view/PhotoListItemView.kt
+++ ui/src/main/java/com/example/ui/main/bottomtabs/view/PhotoListItemView.kt
@@ -1,102 +1,88 @@
 package com.example.ui.main.bottomtabs.view
 
-import android.content.res.Configuration
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil.compose.AsyncImage
+import coil3.compose.AsyncImage
+import coil3.request.ImageRequest
+import coil3.request.crossfade
 import com.example.domain.model.PhotoItem
 import com.example.ui.R
-import com.example.ui.common.SPACING_LARGE
+import com.example.ui.common.SPACING_MEDIUM
 import com.example.ui.common.SPACING_SMALL
 
 @Composable
-internal fun PhotoListItemView(photoItem: PhotoItem, onItemClick: () -> Unit) {
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(SPACING_SMALL.dp)
-            .clickable {
-                onItemClick()
-            },
-        shape = RoundedCornerShape(8.dp),
-        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
-    ) {
-        Column {
+internal fun PhotoListItemView(
+    modifier: Modifier = Modifier,
+    photoItem: PhotoItem,
+    onPhotoClick: () -> Unit
+) {
+    Column(modifier = modifier) {
+        Card(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .background(Color.Transparent)
+                .clickable {
+                    onPhotoClick()
+                },
+            shape = RoundedCornerShape(SPACING_MEDIUM.dp),
+        ) {
+            val context = LocalContext.current
+
             AsyncImage(
-                model = photoItem.url,
-                contentDescription = null,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(200.dp),
-                contentScale = ContentScale.Crop
+                model = ImageRequest.Builder(context)
+                    .data(photoItem.url)
+                    .crossfade(true)
+                    .build(),
+                modifier = Modifier.fillMaxWidth(),
+                contentScale = ContentScale.FillWidth,
+                contentDescription = "Searched photo",
+                placeholder = painterResource(id = R.drawable.ic_placeholder),
+                error = painterResource(id = R.drawable.ic_placeholder)
             )
+        }
+        Text(
+            text = photoItem.title,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(top = SPACING_SMALL.dp)
+        )
 
-            Text(
-                text = photoItem.title,
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(8.dp)
-            )
-
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-
-                if (photoItem.isPublic) {
-                    Image(
-                        modifier = Modifier.size(SPACING_LARGE.dp),
-                        painter = painterResource(id = R.drawable.ic_public),
-                        contentDescription = "Photo public icon",
-                    )
-                }
-
-                if (photoItem.isFriend) {
-                    Image(
-                        modifier = Modifier.size(SPACING_LARGE.dp),
-                        painter = painterResource(id = R.drawable.ic_friends),
-                        contentDescription = "Photo friends icon"
-                    )
-                }
-
-                if (photoItem.isFamily) {
-                    Image(
-                        modifier = Modifier.size(SPACING_LARGE.dp),
-                        painter = painterResource(id = R.drawable.ic_family),
-                        contentDescription = "Photo family icon"
-                    )
-                }
-            }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = SPACING_SMALL.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            PublicIconImage(photoItem.isPublic)
+            FriendsIconImage(photoItem.isFriend)
+            FamilyIconImage(photoItem.isFamily)
         }
     }
 }
 
-@Preview(showBackground = true, showSystemUi = true)
+@Preview(showBackground = true)
 @Composable
-private fun PhotoListItemPreview() {
+private fun PhotoListItemWithAllIconsPreview() {
     PhotoListItemView(
         photoItem = PhotoItem(
             title = "Photo title",
@@ -104,6 +90,22 @@ private fun PhotoListItemPreview() {
             isPublic = true,
             isFriend = true,
             isFamily = true
-        )
-    ) {}
+        ),
+        onPhotoClick = { }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PhotoListItemWithFriendPreview() {
+    PhotoListItemView(
+        photoItem = PhotoItem(
+            title = "Photo title",
+            url = " https://farm66.staticflickr.com/65535/54375913088_62172768d8.jpg",
+            isPublic = false,
+            isFriend = true,
+            isFamily = false
+        ),
+        onPhotoClick = { }
+    )
 }
