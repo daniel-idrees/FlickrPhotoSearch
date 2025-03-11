eb89183ca3a3c8f9f26bd703ce76b66ee5b98a88
Daniyal Idrees <daniyal.idrees@sprind.org>
2025-03-11 20:56:33 +0100

    clear icon view added separately

---
+++ ui/src/main/java/com/example/ui/main/bottomtabs/view/ClearIcon.kt
@@ -0,0 +1,22 @@
+package com.example.ui.main.bottomtabs.view
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun ClearIcon(
+    modifier: Modifier = Modifier,
+    tint: Color = LocalContentColor.current
+) {
+    Icon(
+        imageVector = Icons.Default.Clear,
+        contentDescription = "Clear icon",
+        modifier = modifier,
+        tint = tint
+    )
+}
