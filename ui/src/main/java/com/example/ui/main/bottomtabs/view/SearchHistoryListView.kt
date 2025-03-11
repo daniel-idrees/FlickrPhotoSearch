19af400e1e699e199b2c5edfd082f8a8d4be3d03
Daniyal Idrees <daniyal.idrees@sprind.org>
2025-03-11 23:45:00 +0100

    unused imports removed

--- ui/src/main/java/com/example/ui/main/bottomtabs/view/SearchHistoryListView.kt
+++ ui/src/main/java/com/example/ui/main/bottomtabs/view/SearchHistoryListView.kt
@@ -5,15 +5,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.ui.R
 import com.example.ui.common.SPACING_EXTRA_SMALL
 import com.example.ui.common.SPACING_MEDIUM
@@ -41,8 +37,6 @@ internal fun SearchHistoryListView(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(SPACING_MEDIUM.dp)
     ) {
-
-
         ButtonWithTextView(
             modifier = Modifier
                 .padding(horizontal = SPACING_EXTRA_SMALL.dp)
--- ui/src/main/java/com/example/ui/main/bottomtabs/view/TextView.kt
+++ ui/src/main/java/com/example/ui/main/bottomtabs/view/TextView.kt
@@ -1,6 +1,5 @@
 package com.example.ui.main.bottomtabs.view
 
-import androidx.annotation.StringRes
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
