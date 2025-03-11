01f72c5b7d3e5b3765cfad2b1b46ba8c5e489d8d
Daniyal Idrees <daniyal.idrees@sprind.org>
2025-03-11 16:44:33 +0100

    unused file removed

--- app/src/main/java/SapFlickrExampleApplication.kt
+++
@@ -1,7 +0,0 @@
-package com.example.sapflickrexample
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class SapFlickrExampleApplication: Application()
