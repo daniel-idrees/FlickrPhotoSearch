1bb726404efef80fd5d43e07c4cc29f5e1fb0f22
Daniyal Idrees <daniyal.idrees@sprind.org>
2025-03-11 23:46:07 +0100

    unused imports removed

--- ui/src/main/java/com/example/ui/main/bottomtabs/view/ButtonWithTextView.kt
+++ ui/src/main/java/com/example/ui/main/bottomtabs/view/ButtonWithTextView.kt
@@ -1,10 +1,8 @@
 package com.example.ui.main.bottomtabs.view
 
-import androidx.annotation.StringRes
 import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 
 @Composable
