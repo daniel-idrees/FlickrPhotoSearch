cf6e3ad3c6abcc19b54b872f3f4c1aaa04e065c3
Daniyal Idrees <daniyal.idrees@sprind.org>
2025-03-12 05:12:19 +0100

    old file removed

--- domain/src/main/java/com/example/domain/model/PhotoItem.kt
+++
@@ -1,9 +0,0 @@
-package com.example.domain.model
-
-data class PhotoItem(
-    val title: String,
-    val url: String,
-    val isPublic: Boolean,
-    val isFriend: Boolean,
-    val isFamily: Boolean,
-)
